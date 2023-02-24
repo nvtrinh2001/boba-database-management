76ecf07abbacc3ecaa94d4978a1d760d665b1472
Nguyen Van Trinh <nguyenvantrinh2607@gmail.com>
2023-02-24 10:16:28 +0700

    fix: a lot of things

--- initialize_db.sql
+++ initialize_db.sql
@@ -1,20 +1,25 @@
-drop table users cascade;
 create table users (
    user_id serial primary key,
    first_name varchar(20) not null,
    last_name varchar(20) not null,
    phone_number varchar(10) not null,
-   email varchar(50) not null,
+   email varchar(50) not null unique,
    password varchar(50) not null,
    house_number varchar(10) not null,
    street varchar(20) not null,
    district varchar(20) not null,
-   city varchar(20) not null,
-   user_role int default 1 check (user_role >= 1 and user_role < 4),
-   hourly_rate int default 0 check (hourly_rate >= 0)
+   city varchar(20) not null
 );
 
-drop table orders cascade;
+create table staff (
+   staff_id serial primary key,
+   user_id int not null,
+   hourly_rate int not null,
+   staff_role int default 1 check (staff_role = 1 or staff_role = 2),
+   unique (user_id, staff_role),
+   constraint inherit_fk foreign key (user_id) references users(user_id)
+);
+
 create table orders (
    order_id serial primary key,
    order_status integer DEFAULT 1 check ((order_status >= 0) and (order_status <= 4)),
@@ -31,7 +36,6 @@ create table orders (
    constraint session_fk foreign key (session_id) references sessions(session_id)
 );
 
-drop table items cascade;
 create table items (
    item_id serial primary key,
    category varchar(20) not null,
@@ -40,7 +44,6 @@ create table items (
    item_size integer not null check ((item_size >= 0) and (item_size <= 3))
 );
 
-drop table order_detail ;
 create table order_detail(
    order_id integer not null,
    item_id integer not null,
@@ -51,7 +54,6 @@ create table order_detail(
    constraint order_detail_pk primary key (order_id, item_id)
 )
 
-drop table ingredients cascade;
 create table ingredients (
    ing_id serial primary key,   
    name varchar(20) not null,
@@ -59,7 +61,6 @@ create table ingredients (
    price_per_gram integer not null check (price_per_gram > 0)
 );
 
-drop table item_detail ;
 create table item_detail(
    ingredient_id integer not null,
    item_id integer not null,
@@ -76,17 +77,14 @@ create table sessions (
    shift_id integer not null,
    session_date date not null,
    constraint shift_fk foreign key (shift_id) references shifts(shift_id),
-   constraint staff_fk foreign key (staff_id) references users(user_id)
+   constraint staff_fk foreign key (staff_id) references staff(staff_id)
 );
 
-drop table shifts cascade;
 create table shifts(
    shift_id serial primary key,
    starting_time time not null,
    ending_time time not null, 
    day_of_week integer not null check ((day_of_week >= 2) and (day_of_week <= 8))
 );
-
-select * from sessions s ;
 
 
